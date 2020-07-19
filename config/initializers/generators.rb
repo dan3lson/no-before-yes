d97f89b71668f055e31c93c78ca514a3cef6e8e8
Danelson Rosa <danelson.rosa@gmail.com>
2020-07-19 11:16:34 -0400

    Update generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,6 +2,13 @@
 
 Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
-  g.test_framework :rspec
+  g.test_framework :rspec,
+                   view_specs: false,
+                   routing_specs: false,
+                   request_specs: false,
+                   controller_specs: false
   g.fixture_replacement :factory_bot, dir: 'spec/factories'
+  g.helper false
+  g.jbuilder false
+  g.stylesheets false
 end
