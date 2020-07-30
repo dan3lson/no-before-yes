6842bed82059737e90a3a5b08cc5a0cc34ef2360
Danelson Rosa <danelson.rosa@gmail.com>
2020-07-30 17:10:34 -0400

    Update DashboardPresenter placeholder specs

--- spec/presenters/dashboard_presenter_spec.rb
+++ spec/presenters/dashboard_presenter_spec.rb
@@ -3,7 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe DashboardPresenter do
-	describe '#num_noes'
+	describe '#num_noes' do
+		context 'noes this week'
+		context 'noes last week'
+		context 'zero noes'
+	end
 	describe '#num_noes_needed'
 	describe '#progress'
 end
