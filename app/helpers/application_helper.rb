089a4b196372c2bed921c9c2de0010e6264e9c8b
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-03 00:03:40 -0400

    Style the navbar and show the active page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
 	def format_date(date)
 		date.strftime("%A, %B #{date.day.ordinalize}")
 	end
+
+	def active_link?(controller_name)
+		return unless request.path.include?(controller_name.to_s)
+
+		'active'
+	end
 end
