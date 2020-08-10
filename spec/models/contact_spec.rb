3a5863b9cbd5ba8a7d6592db25ac2d5366a43ebb
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-09 23:31:42 -0400

    Update Contact model spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe Contact, type: :model do
 
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
-    subject { create(:contact) }
-    it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   end
 
   describe 'scopes' do
     describe '::untouched'
+    describe '::overdue'
   end
 
   describe 'instance methods' do
