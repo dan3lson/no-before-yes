dd74e6b9438e533e256e5a5351e2396445431ed2
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-11 07:34:18 -0400

    Add blank About page, linked by "inspired by..."

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'how_it_works', to: 'static_pages#how_it_works'
   get 'pricing', to: 'static_pages#pricing'
   get 'support', to: 'static_pages#support'
+  get 'about', to: 'static_pages#about'
 
   get '/sign_in' => 'clearance/sessions#new', as: 'sign_in'
   delete '/sign_out' => "clearance/sessions#destroy", as: 'sign_out'
