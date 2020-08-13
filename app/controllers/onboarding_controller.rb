0eeed3d14e020e25de3e9cbd45c023e70637aeb5
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-13 16:11:38 -0400

    Add JS to move through onboarding steps

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -5,4 +5,11 @@ class OnboardingController < ApplicationController
   layout 'application_onboarding'
 
   def index; end
+
+  def update
+    @step = params[:step]
+    onboarding_enum = "onboarding_#{@step}!"
+
+    current_user.send(onboarding_enum)
+  end
 end
