52b1e8c3ba3d815882e4bb43bb62c1311f834ccc
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-14 08:43:39 -0400

    Add security layer to onboarding query params

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,20 @@
 
 # DashboardController
 class DashboardController < ApplicationController
+  before_action :set_onboarding_finished
+  
   def show
-    @onboarding_finished = params[:onboarding_finished]
     @dashboard = DashboardPresenter.new(current_user)
+  end
+
+  private
+
+  def set_onboarding_finished
+    @onboarding_finished = dashboard_params[:onboarding_finished] &&
+      dashboard_params[:onboarding_finished] == 'true'
+  end
+
+  def dashboard_params
+    params.permit(:onboarding_finished)
   end
 end
