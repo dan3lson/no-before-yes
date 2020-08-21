80b99095c20c54ce4abe8b87ff2e3a9d1905c9fc
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-21 15:08:51 -0400

    Show visits for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :contacts, dependent: :restrict_with_error
   has_many :touchpoints, dependent: :restrict_with_error
   has_many :goals, dependent: :destroy
+  has_many :visits, class_name: 'Ahoy::Visit'
 
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
 
