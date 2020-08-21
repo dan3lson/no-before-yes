af9dd23bbb04709d51c70784e1d7b590410e2568
Danelson Rosa <danelson.rosa@gmail.com>
2020-08-21 15:09:04 -0400

    Track all events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 	around_action :set_time_zone, if: :current_user
 
+  after_action :track_action
+
+  protected
+
+  def track_action
+    ahoy.track "Ran action", request.path_parameters
+  end
+
 	private
 
 	def set_time_zone
