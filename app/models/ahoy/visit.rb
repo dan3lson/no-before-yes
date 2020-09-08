de6d66bc30cf6be39f011009fb24332ad714cfd4
Danelson Rosa <danelson.rosa@gmail.com>
2020-09-08 16:52:00 -0400

    Set implicit order for db tables

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,5 +1,6 @@
 class Ahoy::Visit < ApplicationRecord
   self.table_name = 'ahoy_visits'
+  self.implicit_order_column = :started_at
 
   has_many :events, class_name: 'Ahoy::Event'
   belongs_to :user, optional: true
