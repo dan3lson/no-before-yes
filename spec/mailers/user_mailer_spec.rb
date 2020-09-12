c167bbbf4fe7f1aaffe9f4aa5282db1d9e0d675f
Danelson Rosa <danelson.rosa@gmail.com>
2020-09-12 09:01:03 -0400

    Organize UserMailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
   let!(:user) { create(:user) }
-  let(:mail) { UserMailer.with(user: user).welcome }
+  let(:mail) { UserMailer.with(user: user) }
   let(:from_address) { 'hello@nobeforeyes.com' }
 
-  it 'renders the headers' do
-    expect(mail.subject).to eq('Thanks for signing up')
-    expect(mail.to).to eq([user.email])
-    expect(mail.from).to eq([from_address])
-  end
+  context '#welcome' do
+    before { mail.welcome }
 
-  it 'renders the body cta' do
-    expect(mail.body.encoded).to match('Get started')
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Thanks for signing up')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq([from_address])
+    end
+
+    it 'renders the body cta' do
+      expect(mail.body.encoded).to match('Get started')
+    end
   end
 end
