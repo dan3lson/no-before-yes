e5c8a78dbde180d6a2fba77ce45ae11a6bd76fef
Danelson Rosa <danelson.rosa@gmail.com>
2020-09-15 14:57:21 -0400

    Add Monday prep mailer to previews

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-
-# UserMailerPreview
 class UserMailerPreview < ActionMailer::Preview
   def welcome
 		UserMailer.with(user: User.first).welcome
+  end
+
+  def monday_prep
+    UserMailer.with(user: User.first).monday_prep
   end
 end
