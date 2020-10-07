e3dbcc4d9e1079f6569dde04b7343a359c26bd98
Danelson Rosa <danelson.rosa@gmail.com>
2020-10-07 19:54:23 -0400

    Resolve prod index migration issue

--- db/migrate/20201007013846_add_unique_index_slug_blog_posts.rb
+++ db/migrate/20201007013846_add_unique_index_slug_blog_posts.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexSlugBlogPosts < ActiveRecord::Migration[6.0]
   def change
-    remove_index :blog_posts, :slug
+    remove_index :blog_posts, :slug if index_exists?(:blog_posts, :slug)
     add_index :blog_posts, :slug, unique: true
   end
 end
